dc8f483197315e81f86ddf1913ec12b14fc992dc
Junior <valdecyborgesjr@hotmail.com>
2024-04-23 19:19:10 -0300

    image addition and route validation

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import App from './App';
 
 const INSTAGRAM_HOSTNAME = 'www.instagram.com';
 
-if (document.location.hostname !== INSTAGRAM_HOSTNAME && false) {
-  alert('Você precisa está no Instagram para executar esse Script');
+if (document.location.hostname !== INSTAGRAM_HOSTNAME) {
+  alert('This app can only run on Instagram routes');
 } else {
   document.title = 'Unfollowers';
   document.body.innerHTML = '<div id="root"></div>';
